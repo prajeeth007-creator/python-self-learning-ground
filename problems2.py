9956f60f0bca2366f979b6d4a45efca98cafacd8
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-07-31 20:24:22 +0530

    i am learning quick

--- problems2.py
+++ problems2.py
@@ -1,9 +1,20 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = (a+b+c)
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#d = (a+b+c)
 
-v = (a*b*c)
+#v = (a*b*c)
 
-n = (d/v)
-print(n)
+#n = (d/v)
+#print(n)
+#next problem 
+
+name = input("enter your name: ")
+score = int(input("enter your score: "))
+department = input("enter your department: ")
+
+c = score / 10
+print("my name is", name)
+print("my score is", c)
+print("my department is", department)
+print(c)
