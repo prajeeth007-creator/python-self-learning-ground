d3ca049905722c732d394f4d0ac579e00710f44c
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-07-31 16:27:23 +0530

    mass pannitan

--- problems2.py
+++ problems2.py
@@ -1,4 +1,12 @@
-name = input()
-age = input()
-print("my name is",name)
-print("my age is",age)
+a = 10
+b = 10 
+c = 10
+d = (a+b+c)
+
+z = 10 
+x = 10
+y = 10 
+v = (z*x*y)
+
+n = (d/v)
+print(n)
