4dfb349aaa02993b77cce3078deea8f5af074dba
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-01 20:04:15 +0530

    Updated nested if examples

--- nestedif,elif.py
+++ nestedif,elif.py
@@ -1,18 +1,18 @@
-#mark = int(input("enter your mark:"))
+mark = int(input("enter your mark:"))
 
-#if(mark == 100):
-   # print("your genius")
-#elif(mark < 70):
-   # print("good try kid")
-#else:
-   # print("better luck next time")
-   
-   
-income = int(input("enter the input:"))
-
-if income < 700:
-    print("your eligible for scholarship")
+if(mark == 100):
+     print("your genius")
+elif(mark < 70):
+    print("good try kid")
 else:
-    print("your not eligible for scholarship")
+    print("better luck next time")
+   
+   
+#income = int(input("enter the input:"))
+
+#if income < 700:
+   # print("your eligible for scholarship")
+#else:
+   # print("your not eligible for scholarship")
     
              
