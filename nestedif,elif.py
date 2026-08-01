11815c30506e6695dcbef5a0c87f77015476d53b
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-01 21:18:43 +0530

    i learned to solve problem which insulted me

--- nestedif,elif.py
+++ nestedif,elif.py
@@ -16,11 +16,18 @@
 #else:
    # print("your not eligible for scholarship")
    
-number = int(input("enter the number:"))
+#number = int(input("enter the number:"))
 
-if number%2 == 0 and number%5 == 0:
-    print("your number is divisible by 2 and 5")
+#if number%2 == 0 and number%5 == 0:
+    #print("your number is divisible by 2 and 5")
+#else:
+   # print("your number is not divisible by 2 and 5")
+   
+number = int(input("enter the number:"))
+if number%2 == 0:
+    print("your number is even")
 else:
-    print("your number is not divisible by 2 and 5")
+    print("your number is odd")
+    
     
              
