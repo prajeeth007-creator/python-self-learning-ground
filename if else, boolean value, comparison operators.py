b5ad9983a420842c9283cf9fcac385f6366cd74a
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-01 11:49:24 +0530

    Added comparison operators lesson

---
+++ if else, boolean value, comparison operators.py
@@ -0,0 +1,12 @@
+#if(True):
+    #print("parthiban")
+#else:
+    #print("leo")
+    
+#print("jingunamani"=="jilla")
+#this is what we call as comparison operator, it will return boolean value either true or false
+agan= "rejects kural"
+if(agan == "rejects kural"):
+    print("kannukulla plays")
+else:
+    print("singari plays")
