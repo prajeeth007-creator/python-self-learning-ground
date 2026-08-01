de1ad2854b88f76e332198f071489bdfd4f8f04e
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-01 11:51:42 +0530

    learning

--- if else, boolean value, comparison operators.py
+++ if else, boolean value, comparison operators.py
@@ -1,12 +1,14 @@
-#if(True):
-    #print("parthiban")
-#else:
-    #print("leo")
-    
-#print("jingunamani"=="jilla")
-#this is what we call as comparison operator, it will return boolean value either true or false
-agan= "rejects kural"
-if(agan == "rejects kural"):
-    print("kannukulla plays")
+if(True):
+    print("parthiban")
 else:
-    print("singari plays")
+    print("leo")
+    
+
+#this is what we call as comparison operator, it will return boolean value either true or false
+
+
+#agan= "rejects kural"
+#if(agan == "rejects kural"):
+    #print("kannukulla plays")
+#else:
+   # print("singari plays")
