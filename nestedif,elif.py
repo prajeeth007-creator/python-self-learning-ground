81d9ac2b4a0640fc0b9a9ac2bba38dbd854bc831
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-01 17:21:06 +0530

    modified nestedif,elif.py

---
+++ nestedif,elif.py
@@ -0,0 +1,18 @@
+#mark = int(input("enter your mark:"))
+
+#if(mark == 100):
+   # print("your genius")
+#elif(mark < 70):
+   # print("good try kid")
+#else:
+   # print("better luck next time")
+   
+   
+income = int(input("enter the input:"))
+
+if income < 700:
+    print("your eligible for scholarship")
+else:
+    print("your not eligible for scholarship")
+    
+             
