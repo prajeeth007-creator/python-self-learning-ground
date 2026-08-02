bf3664ace2f37e8f7dce737db1130b9ddcb9de74
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-02 13:20:20 +0530

    learning

--- learning.py
+++ learning.py
@@ -27,16 +27,13 @@
 salary = int(input("enter your salary:"))
 age = int(input("enter your age:"))
 
-loan = int(input("enter the loan amount:"))
-
-if(loan <= 500000):
-    print("your eligible for loan")
+if (salary > 25000 or age < 25):
+    loan = int(input("enter your loan amount"))
+    if(loan <= 50000):
+        print("your eligible for loan")
+    else:
+        print("maximum amount is 50000")
+    
 else:
-    print("maximum loan amount is 50000")
-
-
-if (salary > 20000 or age <25):
-    print("your eligible for loan")
-else:
-    print("your not eligible for loan")
+    print("your not eligible for loan:")
     
