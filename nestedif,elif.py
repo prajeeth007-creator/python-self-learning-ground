2ad92f7306d46c95206dca7cc01c19727c71bf36
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-02 13:34:44 +0530

    modified few files

--- nestedif,elif.py
+++ nestedif,elif.py
@@ -46,21 +46,18 @@
     # print("good student"
     
     
-a = int(input("enter the number:"))
-b = int(input("enter the number:"))
+tamil = int(input("enter your tamil mark:"))
+english = int(input("enter your english mark:"))
+maths = int(input("enter your maths mark:"))
+science = int(input("enter your science mark:"))
+social = int(input("enter your social mark:"))
 
-operation = input("add/sub/multiply/div:")
-
-if (operation == "add"):
-   print(a+b)
-elif (operation == "sub"):
-   print(a-b)
-elif(operation == "multiply"):
-   print(a*b)
-elif(operation == "div"):
-   print(a/b)
+f = (tamil + english + maths + science + social)/5
+if f>= 35:
+   print("your pass")
 else:
-   print("invalid operation") 
+   print("your fail")
+
    
    
 
