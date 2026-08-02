8f9472553f5c845904d3dec87596051fe8f11a07
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-02 22:19:56 +0530

    learning

--- forloop.py
+++ forloop.py
@@ -2,10 +2,24 @@
    # print(i,"x5=",i*5
 #program ends here ---------------------------------------------------------------------------
     
+#count = 0
+#for i in range(1,11):
+    #if(i%2 == 0):
+       # print(i)
+   # count = count + 1
+
+#print("total even numbers:", count)
+
+#program ends here ---------------------------------------------------------------------------
+
 count = 0
 for i in range(1,11):
     if(i%2 == 0):
-        print(i)
-    count = count + 1
+        print(i,"even number")
+        count = count + 1
+    else:
+        print(i,"odd number")
+        count = count + 1
 
 print("total even numbers:", count)
+print("total odd numbers:", count)
