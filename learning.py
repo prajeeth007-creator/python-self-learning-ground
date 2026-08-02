c9d33e3683122ba17ae92a7afefa53fbdb29862f
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-02 10:50:33 +0530

    learning

--- learning.py
+++ learning.py
@@ -1,15 +1,23 @@
-a = int(input("enter the number:"))
-b = int(input("enter the number:"))
+#a = int(input("enter the number:"))
+#b = int(input("enter the number:"))
 
-operation = input("add/sub/multiply/div:")
+#operation = input("add/sub/multiply/div:")
 
-if (operation=="add"):
-   print(a+b)
-elif (operation=="sub"):
-   print(a-b)
-elif(operation=="multiply"):
-   print(a*b)
-elif(operation=="div"):
-   print(a/b)
+#if (operation=="add"):
+  # print(a+b)
+#elif (operation=="sub"):
+ #  print(a-b)
+#elif(operation=="multiply"):
+ #  print(a*b)
+#elif(operation=="div"):
+#   print(a/b)
+#else:
+#    print("invalid operation")
+
+#program ends here ---------------------------------------------------------------------------
+
+score = int(input("enter your score"))
+if score >= 70:
+    print ("your eligible")
 else:
-    print("invalid operation")
+    print("your not eligible")
