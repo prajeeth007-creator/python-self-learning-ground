13e1ca7edbf213083b3f81d4052684bd6331cb33
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-02 19:42:30 +0530

    learning

--- forloop.py
+++ forloop.py
@@ -1,3 +1,8 @@
-for i in range(1, 11):
-    print(i,"x5=",i*5)
+#for i in range(1, 11):
+   # print(i,"x5=",i*5
+#program ends here ---------------------------------------------------------------------------
     
+for i in range(1,11):
+    if(i%2 == 0):
+        print(i)
+
