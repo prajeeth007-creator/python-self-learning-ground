63448e4c6cce2e286b45f4e7b4e4ca9a093e0af9
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-07 22:07:08 +0530

    learning

--- nestedforloop.py
+++ nestedforloop.py
@@ -1,3 +1,8 @@
-for i in range (1,7):
-    for j in range (1,3):
-        print(j,"apple")
+#for i in range (1,7):
+ #   for j in range (1,3):
+     #   print(j,"apple")
+     
+#program ends here--------------------------------
+
+for j in range(1,4):
+    print("day:",j)    
