a9fe27bb042798516e2804e69f19a4017359f1be
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-09 22:08:39 +0530

    learning

--- functions.py
+++ functions.py
@@ -13,6 +13,18 @@ def sub():
     a=int(input("enter the number:"))
     b=int(input("enter the number"))
     print(a-b)
+    
+def multiple():
+    a = int(input("enter the number:"))
+    b = int(input("enter the number:"))
+    print(a*b)
+    
+def div():
+    a = int(input("enter the number:"))
+    b = int(input("enter the number:"))
+    print(a/b)
+    
 sum ()
 sub ()
-
+multiple()
+div()
