e21d6f9218245490e3c2e5807e82a49209730aca
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-09 14:58:29 +0530

    learning

--- pycollections.py
+++ pycollections.py
@@ -4,6 +4,12 @@
 
 #program ends here ------------------
 
+#a = [1,2,3,4,5,6,7]
+#a.insert(0,1)
+#print(a)
+
+#program ends here--------------------
+
 a = [1,2,3,4,5,6,7]
-a.insert(0,1)
+a[0] = 4
 print(a)
