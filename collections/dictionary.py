02476b7f9ba3560483a1d4d6b13fbf40f21097ff
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-09 17:09:05 +0530

    learning

--- collections/dictionary.py
+++ collections/dictionary.py
@@ -1,4 +1,18 @@
+#----------------------dictionary------------------------
+
+#a = {"hello": "hi",
+ #    "how are you": "i'm fine",
+ #    "what's you age": "20"}
+#print(a["hello"])
+
+
+#here "hello is key" and "hi is value" we can call it key value pair 
+#program ends here----------------------------------------
+
 a = {"hello": "hi",
-     "how are you": "i'm fine",
-     "what's you age": "20"}
-print(a["hello"])
+    "how are you": "i'm fine",
+    "what's you age": "20"}
+
+a["hello"] = "hey"
+
+print(a)
