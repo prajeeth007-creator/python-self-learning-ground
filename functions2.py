fe8076ca6bad72d3afa5ccad7be83555db9ab17c
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-10 23:31:36 +0530

    learning

--- functions2.py
+++ functions2.py
@@ -12,14 +12,23 @@
         
 #a = int(input("enter a:"))
 #oddeven(a)
+#program ends here ------------------------------------
+#def passfail(b):
+ #   if(100 >= b >= 35):
+  #      print("pass")
+   # elif(b<35):
+    #    print("fail")
+   # else:
+  #      print("not defined")
+#a = int(input("enter the mark:"))
+#passfail(a)
 
-def passfail(b):
-    if(100 >= b >= 35):
-        print("pass")
-    elif(b<35):
-        print("fail")
-    else:
-        print("not defined")
-a = int(input("enter the mark:"))
-passfail(a)
+#program ends here --------------------------------------------
+
+def count(x1,x2):
+    for i in range(x1,x2):
+        print(i)
+a = int(input("enter the input:"))
+b = int(input("enter the input"))
+count(a,b)
         
