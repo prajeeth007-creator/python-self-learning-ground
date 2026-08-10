0f6b83e95daf05846504f7d3ba1005ecf906f041
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-10 23:17:12 +0530

    learning

--- functions2.py
+++ functions2.py
@@ -4,11 +4,22 @@
 #painter("spiderman")
 
 
-def oddeven(b):
-    if(b%2 == 0):
-        ptint("this number is even")
-    else:
-        print("this number is odd")
+#def oddeven(b):
+ #   if(b%2 == 0):
+  #      ptint("this number is even")
+  #  else:
+ #       print("this number is odd")
         
-a = int(input("enter a:"))
-oddeven(a)
+#a = int(input("enter a:"))
+#oddeven(a)
+
+def passfail(b):
+    if(100 >= b >= 35):
+        print("pass")
+    elif(b<35):
+        print("fail")
+    else:
+        print("not defined")
+a = int(input("enter the mark:"))
+passfail(a)
+        
