6148d9a184a6275b3e2dd5f8b3743948f51f06ae
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-15 22:04:59 +0530

    learning

--- classesandobjects.py
+++ classesandobjects.py
@@ -11,6 +11,9 @@ tony = goa()
 
 tony.name = "tony"
 steve.name = "steve"
-
+steve.drink = "no"
+tony.drink = "yes"
 print(steve.name)
+print("drink",steve.drink)
 print(tony.name )
+print("drink",tony.drink)
