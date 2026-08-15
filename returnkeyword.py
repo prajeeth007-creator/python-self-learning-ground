e664e59e3653f7d2d94dc6a737a2ff14f9ddab3d
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-15 08:14:18 +0530

    learning

--- returnkeyword.py
+++ returnkeyword.py
@@ -3,17 +3,55 @@
 #a = avengers()
 #print(a)
     
+#program ends here---------------------
     
-name = "top"
-password = 2345
+#name = "top"
+#password = 2345
 
-a = input("enter your name:")
-b = int(input("enter your password:"))
+#a = input("enter your name:")
+#b = int(input("enter your password:"))
 
-def verification():
-    if a == name and b == password :
-        print("correct password and username")
-    else:
-        print("incorrect password or username")
+#def verification():
+ #   if a == name and b == password :
+  #      print("correct password and username")
+   # else:
+    #    print("incorrect password or username")
         
-verification()
+#verification()
+
+#program ends here-----------------------------------
+
+#name = "top"
+#password = 123
+
+#a = input("enter the name:")
+#b = int(input("enter password:"))
+
+#def verification():
+ #   if a == name and b == password:
+  #      return True
+   # else:
+    #    return False
+    
+#a = verification()
+#print(a)
+
+#program ends here --------------------------------------
+
+
+def add(x1,x2):
+    return x1 + x2
+
+x1 = int(input("enter the number:"))
+x2 = int(input("enter the number:"))
+x3 = int(input("enter the number:"))
+
+b = add(x1,x2)
+print(b)
+a = add(x1,x2)
+
+output = a*x3
+
+print(output)
+
+
