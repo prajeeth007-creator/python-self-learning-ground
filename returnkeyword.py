3a7e8fcbe112f43b870a2e4bc86fa6b94a951d92
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-15 07:45:02 +0530

    learning

--- returnkeyword.py
+++ returnkeyword.py
@@ -1,5 +1,19 @@
-def avengers():
-    return "assemble"
-a = avengers()
-print(a)
+#def avengers():
+ #   return "assemble"
+#a = avengers()
+#print(a)
     
+    
+name = "top"
+password = 2345
+
+a = input("enter your name:")
+b = int(input("enter your password:"))
+
+def verification():
+    if a == name and b == password :
+        print("correct password and username")
+    else:
+        print("incorrect password or username")
+        
+verification()
