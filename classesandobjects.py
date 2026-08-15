e407cac06ba04fe1298267e1b4c463d14ea8f105
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-15 13:31:19 +0530

    learning

---
+++ classesandobjects.py
@@ -0,0 +1,16 @@
+class goa:
+    name = ""
+    happy = ""
+    def drink(self):
+        print("lets party")
+    def dance(self):
+        print("lets dance")
+
+steve = goa()
+tony = goa()
+
+tony.name = "tony"
+steve.name = "steve"
+
+print(steve.name)
+print(tony.name )
