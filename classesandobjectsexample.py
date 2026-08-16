64abce4c114680776f9504cd83e04802930773a5
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-16 09:51:13 +0530

    learning

--- classesandobjectsexample.py
+++ classesandobjectsexample.py
@@ -1,23 +1,36 @@
-class student():
+#class student():
+    #def __init__(self):
+     #   self.name = ""
+    #    self.rollnu = 0
+        
+   # def display(self):
+  #      print("name:",self.name)
+ #       print("roll number:",self.rollnu)
+        
+#flute = student()
+#flute.name = "krish"
+#flute.rollnu = 21
+
+#flower = student()
+#flower.name = "radha"
+#flower.rollnu = 22
+
+#flute.display()
+#flower.display()
+
+#program ends here------------------------
+
+
+class colour():
     def __init__(self):
-        self.name = ""
-        self.rollnu = 0
+        colour = "red"
         
     def display(self):
-        print("name:",self.name)
-        print("roll number:",self.rollnu)
+        print("fruit colour:",self.colour)
         
-flute = student()
-flute.name = "krish"
-flute.rollnu = 21
+fruit = colour()
+fruit.colour = "black"
 
-flower = student()
-flower.name = "radha"
-flower.rollnu = 22
-
-flute.display()
-flower.display()
-
-
+fruit.display()
 
     
