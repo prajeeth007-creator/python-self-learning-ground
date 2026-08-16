58c32636aba6faa72569985783029f8c0e3ab3f8
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-16 09:27:55 +0530

    learning

---
+++ classesandobjectsexample.py
@@ -0,0 +1,17 @@
+class student():
+    def __init__(self):
+        self.name = "krish"
+        self.rollnu = 21
+        
+    def display(self):
+        print("name:",self.name)
+        print("roll number:",self.rollnu)
+        
+flute = student()
+flute.name = "krish"
+flute.rollnu = 21
+flute.display()
+
+villain = student()
+flute.name = "kamsa"
+    
