45cc374d37c2788b5c293681a31695393cd4e8ab
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-16 08:34:47 +0530

    learning

---
+++ constructionandselfkeyword.py
@@ -0,0 +1,12 @@
+class avengers():
+    def __init__(self):
+        self.tech = "ironman"
+        self.lead = "captain america"
+        self.god = "thor"
+        
+bigpotatose = avengers()
+bigpotatose.tech ="tony"
+bigpotatose.lead = "steve"
+print(bigpotatose.tech)
+print(bigpotatose.lead)
+        
