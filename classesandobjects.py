c86e3b47c3df2958a5d042f8be592771c6f8ea52
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-08-16 08:57:05 +0530

    learning

--- classesandobjects.py
+++ classesandobjects.py
@@ -1,19 +1,37 @@
-class goa:
-    name = ""
-    happy = ""
-    def drink(self):
-        print("lets party")
-    def dance(self):
-        print("lets dance")
+#class goa:
+    #name = ""
+    #happy = ""
+   # def drink(self):
+  #      print("lets party")
+ #   def dance(self):
+ #       print("lets dance")
 
-steve = goa()
-tony = goa()
+#steve = goa()
+#tony = goa()
 
-tony.name = "tony"
-steve.name = "steve"
-steve.drink = "no"
-tony.drink = "yes"
-print(steve.name)
-print("drink",steve.drink)
-print(tony.name )
-print("drink",tony.drink)
+#tony.name = "tony"
+#steve.name = "steve"
+#steve.drink = "no"
+#tony.drink = "yes"
+#print(steve.name)
+#print("drink",steve.drink)
+#print(tony.name )
+#print("drink",tony.drink)
+
+#program ends here_____________________________
+
+class goa():
+    def __init__ (self):
+        self.lead = "steve"
+        self.tech = "tony"
+        self.tonydrink = "yes drink"
+        self.stevedrink = "no drink"
+        
+name = goa()
+print(name.lead)
+print(name.stevedrink)
+print(name.tech)
+print(name.tonydrink)
+        
+        
+
